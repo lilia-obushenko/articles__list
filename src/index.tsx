85ee6f9b31967563f69033bd5ae2f593fcdf3490
Lilia Obushenko <liliaobushenko@gmail.com>
2023-05-27 21:25:08 +0300

    react version changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import ReactDOM from 'react-dom';
 import { App } from './App';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { HashRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
   <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
   </HashRouter>,
-  document.getElementById('root')
 );
 
